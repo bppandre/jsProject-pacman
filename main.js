afc919595465b05ff31b48566bd3e4de8fac8fa8
Bob Andre <bppa1905@gmail.com>
2020-01-16 19:48:32 -0800

    start/pause functionality

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 let gameData = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 2, 2, 2, 2, 2, 1, 2, 18, 2, 2, 2, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
     [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
     [1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1],
-    [1, 2, 2, 2, 1, 1, 5, 1, 1, 2, 2, 2, 1],
+    [1, 2, 2, 2, 1, 1, 18, 1, 1, 2, 2, 2, 1],
     [1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1],
     [1, 2, 1, 1, 2, 2, 1, 2, 2, 1, 1, 2, 1],
-    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 5, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
 
+// this is a bigger board that needs to be finished 
+// http://3.bp.blogspot.com/_Jrhwx8X9P7g/SwRjAFA8AUI/AAAAAAAAAYM/i9m1fOEY4Os/s1600/Pac-man.png
+// let gameData = [
+//     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+//     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+//     [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+//     [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+//     [1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+//     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+//     [1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+//     [1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1],
+//     [1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1],
+//     [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3, 3, 1, 1, 3, 3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1],
+//     [1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 3, 3, 1, 1, 3, 3, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1],
+// ];
 
 const WALL = 1;
 const COIN = 2;
@@ -26,17 +41,18 @@ const UP_KEY = 38;
 const RIGHT_KEY = 39;
 const DOWN_KEY = 40;
 
+
 let map;
 
 let pacman = {
-    x: 6,
-    y: 4,
+    x: 1,
+    y: 7,
     direction: 'right'
 };
 
 let orangeGhost = {
-    x:8,
-    y:1,
+    x:6,
+    y:4,
     covering:GROUND
 };
 
@@ -64,92 +80,91 @@ function allValidMoves(y,x){
 }
 
 function chase(ghost,target){
+    
     let valid = allValidMoves(ghost.y,ghost.x);
-    console.log(valid);
+    // console.log(valid);
     
     let dist = valid.map(v => getDistance(v,target));
-    console.log(dist);
+    // console.log(dist);
     if(dist.indexOf(0)!==-1){
-        console.log('you lost');
+        // console.log('you lost');
         
     }
     let dir = valid[dist.indexOf(Math.min(...dist))];
-    console.log('min',dir);
+    // console.log('min',dir);
     let x = dir[1];
     let y = dir[0];
-
+    
     gameData[ghost.y][ghost.x] = ghost.covering;
     ghost.covering = gameData[y][x];
     ghost.x = x;
     ghost.y = y;
     gameData[ghost.y][ghost.x] = ORANGE_GHOST;
-    eraseMap();
-    drawMap();
+    
 }
 
-setInterval(()=>chase(orangeGhost, pacman),1000);
 
 
 function createTiles(data) {
-
+    
     let tilesArray = [];
-
+    
     for (let row of data) {
-
+        
         for (let col of row) {
             
             
             let tile = document.createElement('div');
-
+            
             tile.classList.add('tile');
-
+            
             if (col === WALL) {
                 tile.classList.add('wall');
-
+                
             } else if (col === COIN) {
                 tile.classList.add('coin');
-
+                
             } else if (col === BLUE_GHOST) {
-                    tile.classList.add('blueghost');
-
+                tile.classList.add('blueghost');
+                
             } else if (col === RED_GHOST) {
                 tile.classList.add('redghost');
-
+                
             } else if (col === PINK_GHOST) {
                 tile.classList.add('pinkghost');
-
+                
             } else if (col === ORANGE_GHOST) {
                 tile.classList.add('orangeghost');
-
+                
             } else if (col === GROUND) {
                 tile.classList.add('ground');
-
+                
             } else if (col === PACMAN) {
                 tile.classList.add('pacman');
-
+                
                 tile.classList.add(pacman.direction);
-
+                
             }
-
+            
             tilesArray.push(tile);
         }
-
+        
         let brTile = document.createElement('br');
-
+        
         tilesArray.push(brTile);
     }
-
+    
     return tilesArray;
 }
 
 function drawMap() {
     map = document.createElement('div');
-
+    
     let tiles = createTiles(gameData);
     for (let tile of tiles) {
         map.appendChild(tile);
     }
-
+    
     document.body.appendChild(map);
 }
 
@@ -201,41 +216,64 @@ function moveRight() {
     }
 }
 
+function handleKeys(e){
+
+    switch (e.keyCode) {
+
+        case LEFT_KEY:
+            moveLeft();
+            break;
+
+        case UP_KEY:
+            moveUp();
+            break;
+
+        case RIGHT_KEY:
+            moveRight();
+            break;
+
+        case DOWN_KEY:
+            moveDown();
+            break;
+
+        default:
+            break;
+
+    }
+}
+
 function setupKeyboardControls() {
-    document.addEventListener('keydown', function (e) {
+    document.addEventListener('keydown', handleKeys);
+}
 
-        switch (e.keyCode) {
+let startBtn = document.getElementById('start');
+let pauseBtn = document.getElementById('pause');
+let ghostId;
 
-            case LEFT_KEY:
-                moveLeft(); 
-                break;
+function main(){
+    drawMap();
 
-            case UP_KEY:
-                moveUp();
-                break;
+    startBtn.addEventListener('click',()=>{
+        console.log('start');
+        setupKeyboardControls();
+        ghostId = setInterval(() => chase(orangeGhost, pacman), 800);
 
-            case RIGHT_KEY:
-                moveRight();
-                break;
+    });
 
-            case DOWN_KEY:
-                moveDown();
-                break;
+    pauseBtn.addEventListener('click', ()=>{
+        console.log('pause');
+        clearInterval(ghostId);
+        document.removeEventListener('keydown',handleKeys);
+    });
 
-            default:
-                break;
-
-        }
-
+    function loop() {
         eraseMap();
         drawMap();
-    });
+        requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+
+
 }
 
-function main() {
-    drawMap();
-    setupKeyboardControls();
-}
-
-main();
-
+main()
